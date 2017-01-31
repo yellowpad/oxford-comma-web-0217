ce818b94ba1401c70d03951c5e66283524c2c2b4
Zayne Abraham <zayneabraham1@gmail.com>
2017-01-31 06:17:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  sizzler = array.size
+
+  if sizzler == 2
+    array.join(" and ")
+  elsif sizzler == 3
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+  elsif sizzler > 3
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+  else
+    array.join(',')
+  end
 
 end
